3ec587fdfa1a610f9ae82fc1b3628ccf9d58df01
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-25 19:07:11 -0600

    commit tree a312b0b75dabb0803cb397474d79b7a2898bbac8

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  # GET /login
+  def new
+  end
+
+  # POST /login
+  def create
+    if params[:name].blank?
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  # POST /logout
+  def destroy
+    session.delete :name
+  end
+end
